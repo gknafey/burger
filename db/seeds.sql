716ab230772484102924c228a3aef1ac7454115a
Garpken <gknafey@gmail.com>
2019-09-09 10:38:24 -0400

    Added alot of necessary files, did the orm pathing, connection.js, and more

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Cheesy Deluxe Burger');
+INSERT INTO burgers (burger_name) VALUES ('Cheesy Bacon Burger');
+INSERT INTO burgers (burger_name) VALUES ('Quarter Pounder Burger');
