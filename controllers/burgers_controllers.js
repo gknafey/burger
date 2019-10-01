16e5a980117e590ca85dec375fe146efe2daef8c
Garpken <gknafey@gmail.com>
2019-09-30 23:43:19 -0400

    Got everything to work to some degree

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -7,50 +7,41 @@ var burger = require("../models/burger.js");
 router.get("/", function(req, res) {
   burger.all(function(data) {
     var hbsObject = {
-      burger: data
+      burgers: data
     };
     console.log(hbsObject);
     res.render("index", hbsObject);
   });
 });
 
-router.post("/api/burger", function(req, res) {
+router.post("/", function(req, res) {
   burger.create([
-    "name", "sleepy"
+    "burger_name", "devoured"
   ], [
-    req.body.burger_name,
-  ], function(result) {
-    res.json({ id: result.insertId });
+    req.body.burger_name, req.body.devoured
+  ], function() {
+    res.redirect("/");
   });
 });
 
-router.put("/api/burger/:id", function(req, res) {
+router.post("/:id", function(req, res) {
   var condition = "id = " + req.params.id;
 
   console.log("condition", condition);
 
   burger.update({
     devoured: req.body.devoured
-  }, condition, function(result) {
-    if (result.changedRows == 0) {
-      // If no rows were changed, then the ID must not exist, so 404
-      return res.status(404).end();
-    } else {
-      res.status(200).end();
-    }
+  }, condition, function() {
+    res.redirect("/");
   });
 });
 
-router.delete("/api/burgers/:id", function(req, res) {
+
+router.delete("/:id", function(req, res) {
   var condition = "id = " + req.params.id;
 
-  burger.delete(condition, function(result) {
-    if (result.affectedRows == 0) {
-      // If no rows were changed, then the ID must not exist, so 404
-      return res.status(404).end();
-    } else {
-      res.status(200).end();
-    }
+  burger.delete(condition, function() {
+    res.redirect("/");
   });
 });
 
