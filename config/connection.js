00da09557cd83a6f7cd59b56b6a242ff92400372
Garpken <gknafey@gmail.com>
2019-10-19 11:12:59 -0400

    edited the connection

--- config/connection.js
+++ config/connection.js
@@ -2,11 +2,11 @@
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-  host: "s9xpbd61ok2i7drv.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+  host: "s9xpbd61ok2i7drv.cbetxkdyhwsb.us-east-1.rds.amazonaws.com"	 || "localhost",
   port: process.env.PORT || 3306,
-  user: "	e4okyrznbswifoet",
-  password: "	pdkqunpmqub2rxed",
-  database: "dkk8ytitycx85ixf"
+  user: "e4okyrznbswifoet" || "root", 
+  password: "pdkqunpmqub2rxed" || "pass",
+  database: "dkk8ytitycx85ixf" || "burgers_db"
 });
 
 // Make connection.
