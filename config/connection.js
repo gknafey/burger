b83f914c1694721ee92a8c9a2f6477a8aa8d567d
Garpken <gknafey@gmail.com>
2019-10-19 11:23:07 -0400

    edited the connection.js file to work hopefully

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,18 @@
 
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-  host: "s9xpbd61ok2i7drv.cbetxkdyhwsb.us-east-1.rds.amazonaws.com"	 || "localhost",
+if (process.env.JAWSDB_URL) {
+connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+connection = mysql.createConnection({
+  host: "localhost",
   port: process.env.PORT || 3306,
-  user: "e4okyrznbswifoet" || "root", 
-  password: "pdkqunpmqub2rxed" || "pass",
-  database: "dkk8ytitycx85ixf" || "burgers_db"
+  user: "root", 
+  password: "pass",
+  database: "burgers_db"
 });
+};
 
 // Make connection.
 connection.connect(function(err) {
